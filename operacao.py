fa1a1bffd59a9cd003386b6ecb6d45f1f2cb3a2b
renanmonterozz <guimonteiro0710@gmail.com>
2026-03-26 21:00:59 -0300

    Create operacao.py
    operacao

---
+++ operacao.py
@@ -0,0 +1,20 @@
+var_int =
+var_float =
+var_str =
+var_bool =
+
+#operacões aritmeticas 
+var_int = 1+1 
+
+#operação relacional 
+var_bool_1 = 3 == 3 
+var_bool_2 = 3 != 9
+var_bool_3 = 2 < 5
+var_bool_4 = 5 > 1
+var_bool_5 = 2 >= 2
+var_bool_6 = 2 <= 2 
+print(var_bool_3)
+ 
+teste = 0 == False > 4-3
+
+print(teste)

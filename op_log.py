df76ce932e98adb650f643d67eb0c47d9ec8429c
renanmonterozz <guimonteiro0710@gmail.com>
2026-03-31 19:53:43 -0300

    Create op_log.py
    operações logicas dentro do python

---
+++ op_log.py
@@ -0,0 +1,22 @@
+#Operação Logica
+'''or 
+and 
+not''' 
+
+'''print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)'''
+
+'''print(True and True)
+print(False and True)
+print(True and False)
+print(False and False)'''
+
+'''print(not True)
+print(not False)'''
+
+print(1==2 and 3<9 or 5-4 == 1)
+
+#teste = 0 == False > 4-3 
+

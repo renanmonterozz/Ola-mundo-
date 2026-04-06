5d721be8554ebe0140efa18e0e5fca8c650ed318
renanmonterozz <guimonteiro0710@gmail.com>
2026-04-06 20:58:32 -0300

    Create hatlist.py
    hat.list

---
+++ hatlist.py
@@ -0,0 +1,19 @@
+hat_list = [1, 2, 3, 4, 5] # Esta é uma lista atual de números ocultos no hat.
+
+print(len(hat_list))
+
+print(hat_list)
+
+hat_list.insert (2,input())
+
+#print(hat_list)
+
+#hat_list.insert(1, input())
+# Etapa 1: escreva uma linha de código que solicita ao usuário
+# que substitua o número do meio por um número inteiro inserido pelo usuário. 
+
+# Etapa 2: escreva uma linha de código que remova o último elemento da lista.
+
+# Etapa 3: escreva uma linha de código que imprima o comprimento da lista atual
+
+#print (hat_list) 

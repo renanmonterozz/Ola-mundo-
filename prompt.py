265bf104cc47a69b2e3cc03968906e74acc75992
renanmonterozz <guimonteiro0710@gmail.com>
2026-04-23 20:53:47 -0300

    Create prompt.py
    script

---
+++ prompt.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('600x400')
+root.title("snp")
+
+framebranco = tk.Frame(root, bg= "white")
+framebranco.place(rely=0, relx=0, relheight=1.0, relwidth=1.0)
+
+
+root.mainloop()
+
